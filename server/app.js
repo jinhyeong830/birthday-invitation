67f4c1972a6dd29d978bb9745a864815117e5ff2
jinhyeong830 <shg02387@naver.com>
2023-09-25 13:59:36 +0900

    server and client setting

--- server/app.js
+++ server/app.js
@@ -1,7 +1,18 @@
-const express = requre("express");
-const app = express();
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config;
 const PORT = 8000;
+const app = express();
 
 // react node 연동
 // https://codingapple.com/unit/nodejs-react-integration/
 // https://velog.io/@autumndr3ams/210802-React-Node.jsexpress-%EC%97%B0%EA%B2%B0%ED%95%98%EA%B8%B0
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+const dataRouter = require("./routes/data");
+app.use("/data", dataRouter);
+app.listen(PORT, () => {
+  console.log("port open:", PORT);
+});
---
+++ server/mongoConnect.js
@@ -0,0 +1,14 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const DB_URI = "";
+const DB_URI_ATLAS = "";
+
+const uri = DB_URI_ATLAS;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+module.exports = client;
