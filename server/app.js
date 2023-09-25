5d63a33716241f8dea779eb954b549a63eba46db
jinhyeong830 <shg02387@naver.com>
2023-09-25 11:11:18 +0900

    initial setting

---
+++ server/app.js
@@ -0,0 +1,7 @@
+const express = requre("express");
+const app = express();
+const PORT = 8000;
+
+// react node 연동
+// https://codingapple.com/unit/nodejs-react-integration/
+// https://velog.io/@autumndr3ams/210802-React-Node.jsexpress-%EC%97%B0%EA%B2%B0%ED%95%98%EA%B8%B0
