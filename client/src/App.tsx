ddecd4ca4ec0fdddf44d3a3563588984b8c57bcf
jinhyeong830 <shg02387@naver.com>
2023-10-04 10:37:41 +0900

    feat: db initial setting & set env & delete unuse file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,8 @@ import Home from "./page/Home";
 import Present from "./page/Present";
 import GlobalStyle from "./style/GlobalStyle";
 import { Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 function App() {
   return (
     <>
